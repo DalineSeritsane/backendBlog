46cdb82177d7fbea7047ef0be4ceb681bcb21f3c
DalineSeritsane <dalinelerato@gmail.com>
2024-11-10 16:41:43 +0200

    tests

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const userRoutes = require('./routes/users');
 const blogsRoutes = require('./routes/blogs');
+const path = require('path');
 
 
 const app = express();
@@ -12,6 +13,10 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
+//serve static files from the uploads folder
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+//define routes
 app.use('/api/users', userRoutes);
 app.use('/api/blogs', blogsRoutes);
 
